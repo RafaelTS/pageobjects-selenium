af50889741a6979a8f07b0bf7cbc870de2cfcbeb
RafaelTS <rafaeltorress@gmail.com>
2023-04-23 15:41:15 -0300

    configurando o projeto

---
+++ src/main/java/com/pageobjectsselenium/PageobjectsSeleniumApplication.java
@@ -0,0 +1,13 @@
+package com.pageobjectsselenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageobjectsSeleniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageobjectsSeleniumApplication.class, args);
+	}
+
+}
